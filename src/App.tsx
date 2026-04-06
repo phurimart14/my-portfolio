6051908835d5eaedf19a5ae7f347a5863036de82
Your Name <phurimart14@gmail.com>
2026-04-07 00:18:17 +0700

    feat: merge branchHero to main

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Contact } from "./components/Contact";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br form-gray-900 via-black to-gray-900">
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900">
       <Hero />
       <About />
       <Skills />
