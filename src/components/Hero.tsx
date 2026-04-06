a7e3544668bb148c284dc226dc8a100ed6c4c73b
Your Name <phurimart14@gmail.com>
2026-04-06 13:29:10 +0700

    feat: add detail Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ChevronDown, Briefcase, Download } from "lucide-react";
 
 export function Hero() {
   const [text, setText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [loopNum, setLoopNum] = useState(0);
+  const [typingSpeed, setTypeingSpeed] = useState(150);
+
+  const title = ["Frontend Developer", "Creative Designer"];
+
+
+  useEffect(() =>{
+
+  })
 
   return <div>Hero</div>;
 }
